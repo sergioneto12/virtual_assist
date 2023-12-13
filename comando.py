6896987bdf80f70f573d57c5961d2b02dd6d834e
sergioneto12 <sergio.ght14@gmail.com>
2023-12-13 20:39:04 -0300

    adding changes on comandos.py with the inclusion of class of Assistant and removing if-else structure

--- comando.py
+++ comando.py
@@ -2,7 +2,7 @@
     coding: utf-8
 
     created by: Sergio Neto
-    last update: 19/03/2023
+    last update: 13/12/2023
 
     references: https://www.youtube.com/watch?v=36RIoJeV95M
 '''
@@ -20,82 +20,111 @@ import pywhatkit
 # module that imports data from chat gpt api
 from index import busca, image
 
-# setting the audio recognizer from your mic, the machine speaker and the command string
-audio = sr.Recognizer()
-assistant = pyttsx3.init()
-language = locale.getdefaultlocale()[0]
-command = ''
+# ...
 
-def executa_command(language: str):
-    '''
-    This function creates an interaction between user and local machine.
-    It is only triggered by calling its name, Lua.
+class Assistant:
+    def __init__(self, language=None):
+        '''
+        Inicializa a instância da classe Assistant.
 
-    It captures the speech and translate it to string format, and by keywords a response is chosen.
+        Parameters:
+            language (str): O idioma a ser usado (padrão é None para o idioma padrão do sistema).
+        '''
+        self.audio = sr.Recognizer()
+        self.assistant = pyttsx3.init()
+        self.language = language or locale.getdefaultlocale()[0]
 
-    The only parameter is a auto-defined language, captured by the system own language.
+    def executa_command(self) -> str:
+        '''
+        Esta função cria uma interação entre o usuário e a máquina local.
+        É acionada apenas chamando seu nome, Lua.
+        Captura o áudio e o traduz para o formato de string. Uma resposta é escolhida com base em palavras-chave.
+        Em caso de erro, uma exceção geral é impressa para o conhecimento do usuário.
 
-    In case of an error, a general exception is printed to the user knowledge.
-    '''
-    try:
-        with sr.Microphone() as source:
-            print('Ouvindo..')
-            sound = audio.listen(source)
-            command = audio.recognize_google(sound, language=language)
-            command = command.lower()
-            if 'lua' in command:
-                command = command.replace('lua', '')
-                assistant.say(command)
-                assistant.runAndWait()
+        Returns:
+            str: O comando reconhecido em formato de string.
+        '''
+        try:
+            with sr.Microphone() as source:
+                print('Ouvindo..')
+                sound = self.audio.listen(source)
+                command = self.audio.recognize_google(sound, language=self.language)
+                command = command.lower()
 
-            return command
+                if 'lua' in command:
+                    command = command.replace('lua', '')
+                    self.assistant.say(command)
+                    self.assistant.runAndWait()
+
+                return command
+
+        except sr.UnknownValueError:
+            print('Não foi possível entender o áudio')
+        except sr.RequestError as e:
+            print(f'Erro na requisição ao Google: {e}')
+        except Exception as e:
+            print(f'Algo deu errado: {e}')
     
-    except Exception as e:
-        print('There is something wrong, ', e)
+    def command_user(self, command: str):
+        '''
+        A resposta do usuário é feita por esta função.
+        Esta é uma versão de teste com termos em português do Brasil.
+        Algumas opções para criar a resposta incluem:
+        - A primeira forma condicional pesquisa na Wikipedia sobre a resposta.
+        - A segunda abre um vídeo para tocar a música exata solicitada.
+        - A terceira também abre uma instância do navegador YouTube, mas com uma exploração aberta.
+        - Finalmente, temos duas chamadas para o Chat GPT: a primeira para uma imagem e a segunda para todas as perguntas.
 
-def command_user(language: str):
-    '''
-    The user response is done by this function.
-    Note that this is a test with brazilian portuguese terms.
+        Parameters:
+            command (str): O comando fornecido pelo usuário.
+        '''
+        keywords_to_functions = {
+            'procure por': self.search_wikipedia,
+            'toque': self.play_music,
+            'no youtube': self.play_youtube,
+            'imagem': self.show_image,
+            'retrato': self.show_image
+        }
 
-    We have some options to create the response:
-    The first conditional form searches in wikipedia about the response.
-    The second opens a vídeo to play the exact song asked. The third part also opens a youtube browser instance, but with an open exploration.
-    Finally, we have two chat gpt calls: the first to an image and the second for all questions.
+        for keyword, function in keywords_to_functions.items():
+            if keyword in command:
+                function(command)
+                break
+        else:
+            self.default_response(command)
 
-    The parameters is a auto-defined language parameter.
-    '''
-    command = executa_command(language)
-    if 'procure por' in command:
+    def search_wikipedia(self, command: str):
         procurar = command.replace('procure por', '')
-        wikipedia.set_lang(str(language)[0:2])
-        resultado = wikipedia.summary(procurar,2)
+        wikipedia.set_lang(str(self.language)[0:2])
+        resultado = wikipedia.summary(procurar, 2)
         print(resultado)
-        assistant.say(resultado)
-        assistant.runAndWait()
+        self.assistant.say(resultado)
+        self.assistant.runAndWait()
 
-    elif 'toque' in command:
-        musica = command.replace('toque','')
+    def play_music(self, command: str):
+        musica = command.replace('toque', '')
         resultado = pywhatkit.playonyt(musica)
-        assistant.say('Tocando música')
-        assistant.runAndWait()
+        self.assistant.say('Tocando música')
+        self.assistant.runAndWait()
 
-    elif 'no youtube' in command:
-        musica = command.replace('no youtube','')
+    def play_youtube(self, command: str):
+        musica = command.replace('no youtube', '')
         resultado = pywhatkit.playonyt(musica)
-        assistant.say('Esse é o seu resultado')
-        assistant.runAndWait()
+        self.assistant.say('Esse é o seu resultado')
+        self.assistant.runAndWait()
 
-    elif ('imagem' in command or 'retrato' in command):
+    def show_image(self, command: str):
         imagem = image(command)
         webbrowser.open(imagem)
-        assistant.say('Mostrando Imagem')
-        assistant.runAndWait()
+        self.assistant.say('Mostrando Imagem')
+        self.assistant.runAndWait()
 
-    else:
+    def default_response(self, command: str):
         resultado = busca(command)
-        assistant.say(resultado)
-        assistant.runAndWait()
+        self.assistant.say(resultado)
+        self.assistant.runAndWait()
 
-command_user(language)
-
+if __name__ == "__main__":
+    assistant = Assistant()
+    user_command = assistant.executa_command()
+    assistant.command_user(user_command)
